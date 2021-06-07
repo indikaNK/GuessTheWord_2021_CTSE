618357a381fc74e5a7786427efa4db3fdff4d7f3
Indika NK <indikank95@gmail.com>
2021-06-07 19:30:20 +0530

    Added new classes , added folder structure

---
+++ lib/api/RetriveQuestions.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GetQuestion extends StatefulWidget{
+  GetQuestion();
+  @override
+  State<StatefulWidget> createState() {
+    GetQuestionState createState() => GetQuestionState();
+
+  }
+
+}
+
+class GetQuestionState extends State<GetQuestion> {
+  GetQuestionState();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return(Scaffold(
+      body: Card(
+        child: StreamBuilder(
+
+        ),
+      ),
+
+    ));
+  }
+
+}
